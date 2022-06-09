58e29fb28e5fe80f7eb4be75b6d7310e4cc0226e
Nikita Reshetnik <reshetnik.nikita@gmail.com>
2022-06-09 16:06:26 +0300

    Initial commit

---
+++ MusicTagsReader.Cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+void ReadTags(DirectoryInfo musicDirectory, string outputPath)
+{
+    using var fs = new FileStream(outputPath, FileMode.Create);
+    using var sw = new StreamWriter(fs, Encoding.UTF8);
+    try
+    {
+        musicDirectory
+            .GetFiles("*.mp3")
+            .Select(x => TagLib.File.Create(x.FullName))
+            .ToList()
+            .ForEach(x => sw.WriteLine($"{string.Join(',', x.Tag.Performers)} - {x.Tag.Title}"));
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+    Console.WriteLine("Finita");
+}
+ReadTags(new DirectoryInfo(@"E:\Music"), @"E:\output.txt");
